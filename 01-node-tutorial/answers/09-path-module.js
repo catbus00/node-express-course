7d4917af0667e06dd9c535ebbe9abec7dedc0058
catbus00 <catmousekill@gmail.com>
2023-10-06 13:47:03 +0700

    update second file path with 09-path-module.js

--- 01-node-tutorial/answers/09-path-module.js
+++ 01-node-tutorial/answers/09-path-module.js
@@ -4,3 +4,10 @@ const pathJoin = path.join(...pathParts);
 console.log('09-path-module:', pathJoin);
 
 
+console.log('09-path-module:', path.sep)
+const filePath = path.join('/content/', 'subfolder', 'test.txt')
+console.log('09-path-module:', filePath)
+const base = path.basename(filePath)
+console.log('09-path-module:', base)
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
+console.log('09-path-module:', absolute)
