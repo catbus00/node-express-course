df52bbe4e1624211be5de32528ca203e7595704a
catbus00 <catmousekill@gmail.com>
2023-10-06 12:42:11 +0700

    edit 11-fs-async.js

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -6,16 +6,19 @@ writeFile('./temporary/fileB.txt', 'This is line 1\n', (err, result) => {
   if (err) {
     console.log('This error happened: ', err);
   } else {
-    writeFile('./temporary/fileB.txt', 'This is line 2\n', { flag: 'a' }, (err, result1) => {
+    console.log('Line 1');
+    writeFile('./temporary/fileB.txt', 'This is line 2\n', { flag: 'a' }, (err, result2) => {
       console.log('at point 2');
       if (err) {
         console.log('This error happened: ', err);
       } else {
-        writeFile('./temporary/fileB.txt', 'This is line 3\n', { flag: 'a' }, (err, result2) => {
+        console.log('Line 2');
+        writeFile('./temporary/fileB.txt', 'This is line 3\n', { flag: 'a' }, (err, result3) => {
           console.log('at point 3');
           if (err) {
             console.log('This error happened: ', err);
           } else {
+            console.log('Line 3');
             console.log('End');
           }
         });
