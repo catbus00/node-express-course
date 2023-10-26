e34344b480916cc5f9ee001d765ac907067bdac7
catbus00 <catmousekill@gmail.com>
2023-10-26 07:21:41 +0700

    add Express elements in app.js and create index.html

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static('./public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './navbar-app/index.html'))
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('resource not found')
+  })
+
+app.listen(3000, () => {
+    console.log('server listening on port 3000')
+})
