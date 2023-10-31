30c226aea9a65edbbdc57e321308d38df884200a
catbus00 <catmousekill@gmail.com>
2023-10-31 16:50:49 +0700

    clean up unused paths

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,74 +1,11 @@
 const express = require('express')
-const path = require('path')
 const app = express()
-const { products, people } = require('./data');
 const peopleRouter = require('./routes/people');
 
-const logger = function (req, res, next) {
-    next();
-  };
-
 app.use(express.static('./methods-public'))
-
-app.get('/', logger, (req, res) => {
-    res.sendFile(path.join(__dirname, './navbar-app/index.html'))
-})
-
-app.get('/api/v1/test', (req, res) => {
-    res.json({ message: "It worked!" })
-})
-
-app.get('/api/v1/products', (req, res) => {
-    res.json(products);
-});
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use('/api/people', peopleRouter);
-
-app.use("/api/v1/people", peopleRouter);
-
-app.post('/api/v1/people', (req, res) => {
-    if (req.body.name) {
-        people.push({ id: people.length + 1, name: req.body.name });
-        res.status(201).json({ success: true, name: req.body.name });
-    } else {
-        res.status(400).json({ success: false, message: "Please provide a name" });
-    }
-})
-
-app.post('/api/people', (req, res) => {
-    if (req.body.name) {
-        people.push({ id: people.length + 1, name: req.body.name });
-        res.status(201).json({ success: true, name: req.body.name });
-    } else {
-        res.status(400).json({ success: false, message: "Please provide a name" });
-    }
-});
-
-app.post('/login', (req, res) => {
-    if (req.body.name) {
-        people.push({ id: people.length + 1, name: req.body.name });
-        res.status(201).json({ success: true, name: req.body.name });
-    } else {
-        res.status(400).json({ success: false, message: "Please provide a name" });
-    }
-});
-app.get('/api/v1/query', (req, res) => {
-    const userSearch = req.query.search || ""; 
-    const maxPrice = parseFloat(req.query.maxPrice) || Number.MAX_VALUE; 
-    const filteredProducts = products.filter((product) => {
-        const searchMatch = product.name.startsWith(userSearch);
-        const priceMatch = parseFloat(product.price) <= maxPrice;
-        return searchMatch && priceMatch;
-    });
-
-    const slicedProducts = filteredProducts.slice(0, 5);
-
-    res.json(slicedProducts);
-});
-
+app.use('/api/people', peopleRouter)
 app.all('*', (req, res) => {
     res.status(404).send('resource not found')
   })
