2955127d44c4b6b329bae00f3ee2e9592e91ae4c
catbus00 <catmousekill@gmail.com>
2023-10-31 19:12:15 +0700

    add cookie logic in separate route and controller files

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -1,7 +1,6 @@
 const { people } = require('../data');
 
 function getPeople (req, res) {
-    console.log('getPeople handler called');
     res.json(people);
 }
 
@@ -10,38 +9,38 @@ function addPerson (req, res) {
         people.push({ id: people.length + 1, name: req.body.name });
         res.status(201).json({ success: true, name: req.body.name });
     } else {
-        res.status(400).json({ success: false, message: "Please provide a name" });
+        res.status(400).json({ success: false, message: 'Please provide a name' });
     }
 }
 
-function getPersonById(req, res) {
+function getPersonById (req, res) {
     const personId = parseInt(req.params.id);
     const person = people.find(person => person.id === personId);
 
     if (person) {
         res.status(200).json(person);
     } else {
-        res.status(404).json({ message: "Person not found" });
+        res.status(404).json({ message: 'Person not found' });
     }
 }
 
-function updatePerson(req, res) {
+function updatePerson (req, res) {
     const personId = parseInt(req.params.id);
     const personIndex = people.findIndex((person) => person.id === personId);
     
     if (personIndex !== -1) {
         if (req.body.name) {
             people[personIndex].name = req.body.name;
-            res.status(200).json({ success: true, message: "Person updated" });
+            res.status(200).json({ success: true, message: 'Person updated' });
         } else {
-            res.status(400).json({ success: false, message: "Please provide a name" });
+            res.status(400).json({ success: false, message: 'Please provide a name' });
         }
     } else {
-        res.status(404).json({ message: "Person not found" });
+        res.status(404).json({ message: 'Person not found' });
     }
 }
 
-function deletePerson(req, res) {
+function deletePerson (req, res) {
     const personId = parseInt(req.params.id);
     const person = people.find(person => person.id === personId);
     if (person !== -1) {
@@ -50,8 +49,9 @@ function deletePerson(req, res) {
         Array.prototype.push.apply(people, updatedPeople); 
         res.status(204).end();
     } else {
-        res.status(404).json({ message: "Person not found" });
+        res.status(404).json({ message: 'Person not found' });
     }
 }
+
 
 module.exports = { getPeople, addPerson, getPersonById, updatePerson, deletePerson }
